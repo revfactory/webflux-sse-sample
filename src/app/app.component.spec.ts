14d6f328f4a7a395eebada8701982515bc2e24ca
robin-hwang <robin.hwang@kakaocorp.com>
2023-03-03 03:49:22 +0900

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'webflux-sample'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('webflux-sample');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('webflux-sample app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'webflux-sample';
+export class AppComponent implements OnInit {
+  eventSource: EventSource;
+  words: string[] = [];
+
+  constructor(
+    private apiService: ApiService,
+    private cd: ChangeDetectorRef
+    ) {}
+
+  ngOnInit(): void {
+    this.eventSource = new EventSource('http://localhost:8080/random-word');
+    this.eventSource.onmessage = (event: MessageEvent) => {
+      this.words.push(event.data);
+      this.cd.detectChanges();
+    };
+  }
 }
